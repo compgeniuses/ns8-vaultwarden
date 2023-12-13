5bec5f621a2b26c5be8f66d4e5eabbc79b847a61
Martin Otieno <martin@genius.ke>
2023-12-13 23:47:46 +0300

    implement redis and pgsql and defaults

--- build-images.sh
+++ build-images.sh
@@ -39,7 +39,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=ghcr.io/paperless-ngx/paperless-ngx:latest" \
+    --label="org.nethserver.images=docker.io/library/redis:7 docker.io/library/postgres:15 ghcr.io/paperless-ngx/paperless-ngx:latest" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
